bcbdb944251d79b7d8487da3c85cef5f614b0ec4
Nathan <hacklemannathan@gmail.com>
2022-11-08 21:38:56 -0600

    update cors

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
@@ -35,12 +37,16 @@ cartItem_schema = CartItemSchema()
 cartItems_schema = CartItemSchema(many=True)
 
 # Error Handler
+
+
 @app.errorhandler(500)
 def errorMessage(e):
     return jsonify(error=str(e)), 500
 
-# Endpoint to create a cart item
-@app.route('/cart-item', methods=["POST"])
+# Endpoint to create a merch item
+
+
+@app.route('/merch-item', methods=["POST"])
 def addCartItem():
     name = request.json['name']
     pictureUrl = request.json['pictureUrl']
@@ -56,21 +62,27 @@ def addCartItem():
 
     return cartItem_schema.jsonify(cartItem)
 
-# Endpoint to query all cart items
-@app.route("/cart-items", methods=["GET"])
+# Endpoint to query all merch items
+
+
+@app.route("/merch-items", methods=["GET"])
 def getCartItems():
     allCartItems = CartItem.query.all()
     result = cartItems_schema.dump(allCartItems)
     return jsonify(result)
 
-# Endpoint to query single cart item
-@app.route("/cart-item/<id>", methods=["GET"])
+# Endpoint to query single merch item
+
+
+@app.route("/merch-item/<id>", methods=["GET"])
 def getCartItem(id):
     cartItem = CartItem.query.get(id)
     return cartItem_schema.jsonify(cartItem)
 
-# Endpoint to update a cart item
-@app.route('/cart-item/<id>', methods=["PUT"])
+# Endpoint to update a merch item
+
+
+@app.route('/merch-item/<id>', methods=["PUT"])
 def updateCartItem(id):
     cartItem = CartItem.query.get(id)
     name = request.json['name']
@@ -86,14 +98,46 @@ def updateCartItem(id):
     db.session.commit()
     return cartItem_schema.jsonify(cartItem)
 
-# Endpoint to delete a cart item
-@app.route('/cart-item/<id>', methods=["DELETE"])
+# Endpoint to delete a merch item
+
+
+@app.route('/merch-item/<id>', methods=["DELETE"])
 def deleteCartItem(id):
     cartItem = CartItem.query.get(id)
     db.session.delete(cartItem)
     db.session.commit()
 
-    return "Cart Item was successfully deleted"
+    return "Merch Item was successfully deleted"
+
+# Endpoint to query for shirts
+
+
+@app.route('/merch-items/shirt', methods=["GET"])
+def getShirts():
+    request = CartItem.query.filter_by(type='shirt')
+    return cartItems_schema.jsonify(request)
+
+# Endpoint to query for pants
+
+
+@app.route('/merch-items/pants', methods=["GET"])
+def getPants():
+    request = CartItem.query.filter_by(type='pants')
+    return cartItems_schema.jsonify(request)
+
+# Endpoint to query for misc
+
+
+@app.route('/merch-items/misc', methods=["GET"])
+def getMisc():
+    request = CartItem.query.filter_by(type='misc')
+    return cartItems_schema.jsonify(request)
+
+
+@app.route('/', methods=["GET"])
+def test():
+    request = CartItem.query.filter_by(type='shirt')
+    return cartItems_schema.jsonify(request)
 
 
 if __name__ == '__main__':
